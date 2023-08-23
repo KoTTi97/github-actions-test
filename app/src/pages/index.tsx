6629dc87148365143c98752d957f5e3bbb70e0b3
Kotti <christian@dualmeta.io>
2023-08-24 01:57:56 +0200

    test

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: FunctionComponent = () => (
       justifyContent: "center",
       width: "100%",
     }}>
-    <h1 style={{ fontSize: 50 }}>Hello world</h1>
+    <h1 style={{ fontSize: 50 }}>Hello world!</h1>
   </div>
 );
 export default Home;
