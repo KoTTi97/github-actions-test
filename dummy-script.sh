55c672166dcfb8622bfcde3dc263b7ef4b525a95
Kotti <christian@dualmeta.io>
2023-08-24 02:26:24 +0200

    test

--- dummy-script.sh
+++ dummy-script.sh
@@ -3,5 +3,7 @@
 # Your script logic here
 my_string="This is the string you want to capture"
 
+echo "Value of VERCEL_CI_TOKEN: $VERCEL_CI_TOKEN"
+
 # Set the output using set-output
 echo "::set-output name=my_output::$my_string"
